e4a95427ea24f34f99ebaf7f29c3ad06398433f0
briskt <3172830+briskt@users.noreply.github.com>
2026-07-17 13:30:38 +0800

    upgrade Cloudflare provider to version 5

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 
 terraform {
-  required_version = ">= 1.0"
+  required_version = "~> 1.8"
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = ">= 3.23, < 5.0.0"
+      version = "~> 5.0"
     }
   }
 }
